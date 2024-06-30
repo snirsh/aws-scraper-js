f2b8b3ae863719b7b185ba60491d4592a75edcde
Snir Sharristh <snirs@wix.com>
2024-06-30 15:55:18 +0300

    fix extreaction

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 export const extractJobs = async (event, context) => {
-    const BASE_URL = "https://careers.wix.com/positions?page=1000"
+    const BASE_URL = "https://careers.wix.com/positions?page=100"
     const response = await fetch(BASE_URL)
     const body = await response.text()
     const regex = /<p class="font_3 .*">(.*?)<\/span><\/p>/g
