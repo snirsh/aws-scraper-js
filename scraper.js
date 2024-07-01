a2caaa1809b7836e952d6b7520023cc9fba46919
Snir Sharristh <snirs@wix.com>
2024-07-01 14:03:13 +0300

    using cheerio

--- scraper.js
+++ scraper.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch'
+import { load } from 'cheerio'
+
+const BASE_URL = "https://careers.wix.com/positions?page=100"
 
 export const extractJobs = async (event, context) => {
-    const BASE_URL = "https://careers.wix.com/positions?page=100"
     const response = await fetch(BASE_URL)
     const body = await response.text()
-    const regex = /<p class="font_3 .*">(.*?)<\/span><\/p>/g
-    return Array.from(body.matchAll(regex)).map(match => match[1])
+    const $ = load(body)
+    return $('.font_3').map((i, el) => $(el).text()).get()
 }
