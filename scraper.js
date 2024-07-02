42a843e4692304518a7bac4d38cbafe740100143
Snir Sharristh <snirs@wix.com>
2024-07-02 08:15:42 +0300

    add node version

--- scraper.js
+++ scraper.js
@@ -1,9 +1,32 @@
-import fetch from 'node-fetch'
+import chromium from 'chrome-aws-lambda'
 
 export const extractJobs = async (event, context) => {
     const BASE_URL = "https://careers.wix.com/positions?page=100"
-    const response = await fetch(BASE_URL)
-    const body = await response.text()
-    const regex = /<p class="font_3 .*">(.*?)<\/span><\/p>/g
-    return Array.from(body.matchAll(regex)).map(match => match[1])
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+    const page = await browser.newPage();
+
+    await page.goto(BASE_URL);
+
+    const jobs = [];
+
+    await page.evaluate(() => {
+        const elements = document.querySelectorAll('a[aria-label="see position"]');
+
+
+        elements.forEach(element => {
+            jobs.push({
+                title: element.closest('p[class^="font_3"]').innerText,
+                location: element.closest('p[class^="font_8"]').innerText,
+                link: element.href
+            });
+        })
+    })
+
+    return jobs
 }
+
